2f97e7d2c09510dbfccbcc8ac5c210a04f29a750
Karen Akopdzhanian <karen.akopdzhanian@gmail.com>
2023-11-02 15:16:44 +0200

    Made changes in temp.py and add new app

---
+++ factorial_for.py
@@ -0,0 +1,12 @@
+input = int(input("Enter a positive number to calculate fractional: "))
+fract = 1 #None
+i = 1
+
+for i in range(i, input+1):
+    print(fract)
+    fract *= i
+
+"""     if fract == None:
+        fract = i """
+    
+print(fract)
